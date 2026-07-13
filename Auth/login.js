f66f055a2efb6d564d3f72222eb38635f2223b72
Abhinandan-Maurya85 <vivekmaurya8509@gmail.com>
2026-07-13 10:58:05 +0530

    Added authentication page components

---
+++ Auth/login.js
@@ -0,0 +1,10 @@
+const Login = () => {
+ return (
+   <div>
+     <h2>Login Page</h2>
+     <p>User login form will be implemented here.</p>
+   </div>
+ );
+};
+
+export default Login;
--- Auth/signup.jsx
+++ Auth/signup.jsx
@@ -3,6 +3,7 @@ const Signup = () => {
     <div>
       <h2>Signup Page</h2>
       <p>User registration form will be implemented here.</p>
+      <p>User registration form will be implemented here with very good content</p>
     </div>
   );
 };

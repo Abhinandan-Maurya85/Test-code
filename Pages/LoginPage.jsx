cdd3e12b0ed9063c5b305259e99d685884981a35
Mohak Jain <jmohak30@gmail.com>
2026-07-13 10:52:31 +0530

    Add loginPage in pages

---
+++ Pages/LoginPage.jsx
@@ -0,0 +1,10 @@
+const Signup = () => {
+  return (
+    <div>
+      <h2>Login Page</h2>
+      <p>User registration form will be implemented here.</p>
+    </div>
+  );
+};
+
+export default Login;

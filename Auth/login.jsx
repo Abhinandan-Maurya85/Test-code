bfd4b7cea4fe87d9fbd383981c65ea6cc013d60b
Mohak Jain <jmohak30@gmail.com>
2026-07-13 11:31:56 +0530

    Write lorem

--- Auth/login.jsx
+++ Auth/login.jsx
@@ -3,6 +3,16 @@ const Signup = () => {
     <div>
       <h2>Signup Page</h2>
       <p>User registration form will be implemented here.</p>
+
+
+      {/* Mohak Jain */}
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam auctor, nisl eget ultricies tincidunt, nunc nisl aliquam nisl, eget ultricies nunc nisl eget nunc. 
+        Lorem ipsum dolor sit amet, consectetur adipi
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam auctor, nisl eget ultricies tincidunt, nunc nisl aliquam nisl, eget ultricies nunc nisl eget nunc. 
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam auctor, nisl eget ultricies tincidunt, nunc nisl aliquam nisl, eget ultricies nunc nisl eget nunc. 
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam auctor, nisl eget ultricies tincidunt, nunc nisl aliquam nisl, eget ultricies nunc nisl eget nunc. 
+      </p>
     </div>
   );
 };
